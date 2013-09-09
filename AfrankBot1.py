e97095f4a82a274ba1c3d923e86a7137045f837e
Andrej Frank <andrej.frank@stz-softwaretechnik.de>
2013-09-09 15:30:25 +0200

    add time meassurment, is buggy

--- AfrankBot1.py
+++ AfrankBot1.py
@@ -1,32 +1,43 @@
 from DuckwarsApi import *
 
 import math
+import time
 
 def calculateDistance(source, destination):
     dx = source.getX() - destination.getX();
     dy = source.getY() - destination.getY();
-    return int(math.ceil(math.sqrt(dx * dx + dy * dy)))
-        
+    return int(dx * dx + dy * dy)
+
 def getNearestCamp(mycamp, camps):
     c=None
     distance=9999999
     for o in camps:
-        d=calculateDistance(mycamp,o)
-        if(d<distance):
+        if((d<distance)and (d>0)):
             c=o
             distance=d
     return c
 
+def sendHalfMenIfFull(st, fromCamp, toCamp):
+    if(fromCamp.getMancount()>(fromCamp.getMaxMancount()-2)):
+        sendMen = fromCamp.getMancount()/2
+        st.issueOrder(fromCamp, toCamp, sendMen)
+
 class AfrankBot1(IBot):
     def doTurn(self, gamestate):
+        t1 = time.time()
         myCamps = gamestate.getMyCamps()
         for c in myCamps:
             camps = gamestate.getNotMyCamps()
             n = getNearestCamp(c, camps)
             if(n!=None):
-                if(c.getMancount()>(c.getMaxMancount()-2)):
-                    sendMen = c.getMancount()/2
-                    gamestate.issueOrder(c, n, sendMen)
+                sendHalfMenIfFull(gamestate, c, n)
+            else:
+                n = getNearestCamp(c, myCamps)
+                if(n!=None):
+                    sendHalfMenIfFull(gamestate, c, n)
+        t2 = time.time()
+        d = (t2-t1)*1000000
+        print "%d us"%d
     
     def getName(self):
         return "AfrankBot1"
