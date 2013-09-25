2c247390a183ee8174897f9cc44d29d720e48db6
Andrej Frank <andrej.frank@stz-softwaretechnik.de>
2013-09-25 14:33:23 +0200

    implement Data class, init+parse

--- Data.py
+++ Data.py
@@ -1,3 +1,6 @@
+import math
+
+# This constants represent the indexes in camps and army array.
 # Camp constants
 C_X=0
 C_Y=1
@@ -14,20 +17,27 @@ A_SRC=2
 A_DST=3
 A_TRIP=4
 A_REM=5
+#
+S_ARMY=0
+S_RET=1
 
 def get_dist(x1, y1, x2, y2):
+    ''' this is a simple and fast destanation length '''
     d = (x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)
     if d == 0:
         return 9999999
     return d
 
 class Data:
-    ''' Data speichert alle Daten ueber Camps (inhalt) 
-        und Armeen die zu Camps kommen'''
+    '''Data store all data about camps (x,y,count, size..)
+       and armies, which are incomming to camp. 
+       the it store all distances to each camp.
+       all datas are simple arrays'''
     def __init__(self, gs):
         ''' create an array of camps, calculate all distances'''
         self.camps=[]
         campslen = gs.getNumCamps()
+        self.send=[] # send store [army to send, result of simulation]
         
         for id in range(campslen):
             c=[0,0,0,0,0,0,[],[]]
@@ -46,7 +56,6 @@ class Data:
             for i in range(campslen):
                 d.append(get_dist(c[C_X], c[C_Y], self.camps[i][C_X], self.camps[i][C_Y]))
             self.camps[id][C_DIST]=d
-
 
     def parse(self, gs):
         '''parse some changed parameters of camp and create armies arrays for each attacked camp'''
@@ -67,6 +76,21 @@ class Data:
             a[A_TRIP]=army.getTripDuration()
             a[A_REM]=army.getTurnsRemaining()
             self.camps[a[A_DST]][C_ARMY].append(a)
+
+    def calculateDistance(self, srcid, dstid):
+        ''' real destanation length '''
+        diff = self.camps[srcid][C_DIST][dstid]
+        return int(math.ceil(math.sqrt(diff)))
+
+    def sendArmy(self, srcid, dstid, cnt):
+        a=[0,0,0,0,0,0,0]
+        a[A_OWNER]=1
+        a[A_CNT]=cnt
+        a[A_SRC]=srcid; a[A_DST]=dstid
+        trip = self.calculateDistance(srcid, dstid)
+        a[A_TRIP]=trip
+        a[A_REM]=trip
+        return a
 
     def __str__(self):
         return str(len(self.camps))+str(self.camps)
