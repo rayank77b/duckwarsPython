b689967b337fb3e483c447d6cca46dce98619a60
Andrej Frank <andrej.frank@stz-softwaretechnik.de>
2013-09-26 15:38:20 +0200

    we must reset data.send array! some logs added

--- AfrankBot2.py
+++ AfrankBot2.py
@@ -30,30 +30,38 @@ class AfrankBot2(IBot):
         # first step, init data
         if self.data==None:
             self.data = Data(gamestate)
-
         self.data.parse(gamestate)
+        # reset the send array
+        self.data.send=[]
         
         # set alle bots
         bots=[]
         bots.append(NextBot(self.data))
-        self.logme("botslen: %d\n"%(len(bots)))     
+        #self.logme("botslen: %d\n"%(len(bots)))     
         for bot in bots:
             bot.calc()
         results=[]
+        
+        #self.logme("sendlen: %d\n"%len(self.data.send))
+        t1a = time.time()
         for s in self.data.send:
             #print s
             sim = Simulation(self.data, s)
             sim.run()
             results.append(sim.get())
         #print results
+        t1b = time.time()
         armies = self.data.getBest(results)
-        t2=time.time()
+        
+        t2=time.time()  ####################
+        
         for a in armies:
             gamestate.issueOrder(gamestate.getCamp(a[A_SRC]), gamestate.getCamp(a[A_DST]), a[A_CNT])
         t3=time.time()
         d1=(t2-t1)*1000
-        d2=(t3-t1)*1000
-        self.logme("t %d: %d   %d\n"%(self.round, int(d1), int(d2)))
+        d1b=(t1b-t1a)*1000
+        d2=(t3-t2)*1000
+        self.logme("t %d: %d %d %d \n"%(self.round, int(d1), int(d1b), int(d2)))
         
     def getName(self):
         return "AfrankBot2"
