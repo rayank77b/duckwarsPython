5fa3c8053ae3404091bbe34e52ed6c8982b3e2da
Andrej Frank <Andrej.Frank@stz-softwaretechnik.de>
2014-11-25 21:44:49 +0100

    neue Duckwars Version, Api hat sich geaendert. Api angepasst, jetzt muss nur noch testApi geschrieben werden

--- DuckwarsApi.py
+++ DuckwarsApi.py
@@ -5,19 +5,20 @@ import math
 class Camp:
     """Repraesentiert ein Camp."""
 
-    def __init__(self,id, campOwner, campMancount, campSize, posX, posY):
+    def __init__(self,id, campOwner, campMancount, campSize, posX, posY, state):
         """Konstruktor"""
         self.__id = id
         self.__owner = campOwner
         self.__mancount = campMancount
         if (campSize < 1):
             campSize = 1
-        if (campSize > 5):
-            campSize = 5
+        if (campSize > state.getSettings().MaxCampSize):
+            campSize = state.getSettings().MaxCampSize
         self.__size = campSize
         self.__x = posX
         self.__y = posY
-    
+        self.__gameState = state
+
     def getID(self):
         """Liefert die ID des Camps. """
         return self.__id
@@ -25,24 +26,41 @@ class Camp:
     def getMancount(self):
         """Liefert die Mannstaerke des Camps. """
         return self.__mancount
-    def setMancount(self, cnt):
+
+    def setMancount(self, count):
         """Aendert die Mannstaerke des Camps."""
-        self.__mancount=cnt
+        self.__mancount=count
         
     def getMaxMancount(self):
         """Liefert die maximale Anzahl Maenner die dieses Camp aufnehmen kann. """
-        return self.__size * 20
-    
+        return self.__gameState.getSettings().MaxManCount[self.__size - 1]
+
+    def getMaxGrowthrate(self):
+        """Liefert die maximale Wachstumsrate der die Anzahl Maenner pro Runde in diesem Camp steigen kann."""
+        return self.__gameState.getSettings().GrowthRate[size - 1]
+
     def getGrowthrate(self):
         """Liefert die Wachstumsrate um der die Anzahl Maenner pro Runde steigt. """
-        if (self.__owner == 0):
-            return 0
-        if (self.__mancount == self.getMaxMancount()):
-            return 0
+        if (self.__mancount < self.getMaxMancount()):
+            x = self.__gameState.getSettings().MaxCampSize - 2
+            for x in range(x,0,-1):
+                if (self.__mancount >= self.__gameState.getSettings().MaxManCount[i - 1]):
+                    return self.__gameState.getSettings().GrowthRate[i]
+            return self.__gameState.getSettings().GrowthRate[0]
         if (self.__mancount > self.getMaxMancount()):
-            return -1
-        return 1 + self.__mancount/20
-    
+            return -self.__gameState.getSettings().OverfullCampDecreseRate
+        return 0
+
+    def getUpgradeCost(self):
+        """Aufruestkosten ermitteln"""
+        if (self.__size < self.__gameState.getSettings().MaxCampSize):
+            return self.__gameState.getSettings().UpgradeCost[self.__size - 1]
+        return -1
+
+    def getCanBeUpgraded(self):
+        """Kann ein Camp aufgeruestet werden?"""
+        return (size < self.__gameState.getSettings().MaxCampSize) and (self.__mancount >= self.getUpgradeCost())
+
     def getOwner(self):
         """Liefert die ID des Spielers dem dieses Camp gehoert. """
         return self.__owner
@@ -50,7 +68,11 @@ class Camp:
     def getSize(self):
         """Liefert die Groesse des Camps (1-5). """
         return self.__size
-    
+
+    def setSize(self, newSize):
+        """Setzt die Groesse des Camps (1-5)."""
+        self.__size = newSize
+
     def getX(self):
         """Liefert die X-Koordinate des Camps """
         return self.__x
@@ -63,9 +85,10 @@ class Camp:
     def __str__(self):
         return "Camp id:%d owner:%d man:%d size:%d (%d:%d)"%(self.__id,self.__owner,self.__mancount,self.__size,self.__x, self.__y)
 
+
 class Army:
     """Repraesentiert eine Armee die auf dem Weg zu einem anderen Camp ist."""
-    
+
     def __init__(self, armyOwner, armySize, armySource, armyDestination, tripLength, remaining):
         """Konstruktor"""
         self.__owner = armyOwner
@@ -94,21 +117,70 @@ class Army:
     def getTripDuration(self):
         """Liefert die Reisedauer. """
         return self.__tripDuration
-    
-    
+
     def getTurnsRemaining(self):
         """Liefert die verbleibende Reisezeit. """
         return self.__turnsRemaining
 
+################################################################################
+#
+class GameSettings:
+    """Enthaelt die Einstellungen fuer das laufende Spiel."""
+    
+    def GameSettings(self):
+        self.MaxCampSize = 5;
+        self.GrowthRate = [1, 2, 3, 4, 5]
+        self.MaxManCount = [20, 40, 60, 80, 100]
+        self.UpgradeCost = [10, 30, 50, 70]
+        self.TravelCost = 1
+        self.TravelCostStartsWithTurn = 7
+        self.OverfullCampDecreseRate = 1
+        self.MaxGameLength=0
+
+    @staticmethod
+    def fromDataString(dataString):
+        x=0
+        data = dataString.split(" ")
+        settings = GameSettings()
+        settings.MaxGameLength = int(data[x])
+        x=x+1
+        settings.OverfullCampDecreseRate = int(data[x])
+        x=x+1
+        settings.TravelCost = int(data[x])
+        x=x+1
+        settings.TravelCostStartsWithTurn = int(data[x])
+        x=x+1
+        settings.MaxCampSize = int(data[x])
+        x=x+1
+        for i in range(settings.MaxManCount.length):
+            settings.MaxManCount[i] = int(data[x])
+            x=x+1
+        for i in range(settings.GrowthRate.length):
+            settings.GrowthRate[i] = int(data[x])
+            x=x+1
+        for i in range(settings.UpgradeCost.length):
+            settings.UpgradeCost[i] = int(data[x]);
+            x=x+1
+        return settings
+
 class GameState:
     """Diese Klasse repraesentiert den aktuellen Spielzustand."""
-
-    def __init__(self, gameStateString):
+    
+    def __init__(self, gameStateString, teamnames, settings):
         """Zur Initialisierung muss ein Spielzustandstring uebergeben werden."""
         self.__armies=[]
         self.__camps=[]
+        self.__teamnames = teamnames
+        self.__settings = settings
         self.__parseGameState(gameStateString)
 
+    def getTeamNames(self):
+        """Liefert die Teamnamen zurueck."""
+        return self.__teamnames
+
+    def getSettings(self):
+        """Liefert die fuer dieses Spiel geltenden Einstellungen."""
+        return self.__settings
 
     def calculateDistance(self, source, destination):
         """Ermittelt den Abstand zwischen zwei Camps, aufgerundend zur naechsten
@@ -117,7 +189,17 @@ class GameState:
         dx = source.getX() - destination.getX();
         dy = source.getY() - destination.getY();
         return int(math.ceil(math.sqrt(dx * dx + dy * dy)))
-    
+
+    def calculateTravelCost(self, source, destination):
+        """ Berechnet die Kosten die eine Reise zwischen den beiden Camps
+            verursachen. Mit Kosten ist gemeint, dass ab einem bestimmten
+            Zug die Entenarmee nach und nach schrumpft.
+        """
+        dist = self.calculateDistance(source, destination)
+        cost = (dist - self.__settings.TravelCostStartsWithTurn) * self.__settings.TravelCost
+        if (cost < 0):
+            cost = 0
+        return cost
 
     def finishTurn(self):
         """Beendet den aktuellen Zug."""
@@ -129,7 +211,7 @@ class GameState:
            mit der 0. Achtung: Die ID kann sich von Zug zu Zug aendern, da sie nicht
            eindeutig vergeben wird. """
         return self.__armies[id];
-        
+
     def getCamp(self, id):
         """Ermittelt das Camp mit der angegebenen ID. Das erste Camp beginnt dabei
            mit der 0. Die IDs sind fuer das ganze Match eindeutig. """
@@ -138,7 +220,7 @@ class GameState:
     def getCamps(self):
         """Liefert eine Liste aller Camps. """
         return self.__camps
-    
+
     def getHostileCamps(self):
         """Ermittelt alle Camps der gegnerischen Spieler. D.h. die eigenen und
            neutralen Camps sind nicht enthalten. """
@@ -147,7 +229,7 @@ class GameState:
             if(p.getOwner() >=2):
                 r.append(p)
         return r
-    
+
     def getMyArmies(self):
         """Ermittelt alle eigenen Truppen. """
         r = []
@@ -155,7 +237,7 @@ class GameState:
             if (army.getOwner() == 1):
                 r.append(army)
         return r
-    
+
     def getMyCamps(self):
         """Ermittelt alle eigenen Camps. """
         r = []
@@ -217,7 +299,7 @@ class GameState:
             if( f.getOwner() == playerID ):
                 return True
         return False
-    
+
     def issueOrder(self, source, dest, mancount):
         """Sendet eine Truppe von einem Camp zu einem anderen. Pro Zug koennen
            beliebig viele Truppenbewegungen gestartet werden. Eine Truppenbewegung
@@ -225,9 +307,16 @@ class GameState:
         source.setMancount(source.getMancount() - mancount)
         dist = self.calculateDistance(source, dest)
         self.__armies.append(Army(1, mancount, source.getID(), dest.getID(), dist, dist))
-        print "%d %d %d"%(source.getID(), dest.getID(), mancount);
+        print "MV %d %d %d"%(source.getID(), dest.getID(), mancount);
         sys.stdout.flush()
-    
+
+    def upgradeCamp(self, dest):
+        """Ein Camp auf die naechste Stufe aufruesten."""
+        dest.setMancount(dest.getMancount() - dest.getUpgradeCost())
+        dest.setSize(dest.getSize() + 1)
+        print "UP %d"%dest.getID()
+        sys.stdout.flush()
+
     def __parseGameState(self, s):
         """Wird verwendet um den Spielstand zu parsen. """
         self.__camps=[]
@@ -268,25 +357,33 @@ class IBot:
         """Wird aufgerufen um den Namen des Bots abzufragen."""
         raise NotImplementedError("Please Implement this method")
 
+# TODO: FIXME
 class Helper:
     """Hilfsklasse welche die Verarbeitungsschleife implementiert."""
-
     @staticmethod
     def executeBot(bot):
         """Durch den Aufruf wird die Verarbeitungsschleife gestartet."""
         try:
+            teamnames=["", "", "", ""]  # 4 teamnames
             line = ""
             message = ""
+            settings = GameSettings()
             line=sys.stdin.readline()
             while(line):
                 if( "go" in line ):
-                    gs = GameState(message)
+                    gs = GameState(message, teamnames, settings)
                     bot.doTurn(gs)
                     gs.finishTurn()
                     message=""
                 elif( "name?" in line ):
                     print bot.getName()
                     sys.stdout.flush()
+                elif( "TN" in line ):
+                    teamname = line.split(":")[1].trim()
+                    id = int(line.substring(2, 3)) - 1;      ## FIXME
+                    teamnames[id] = teamname;
+                elif(line.startsWith("ENV")):
+                    settings = GameSettings.fromDataString(line.substring(4).trim());  ## FIXME
                 else:
                     message=message+line+os.linesep
                 line=sys.stdin.readline()
